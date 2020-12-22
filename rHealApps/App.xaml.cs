eaea5896a467e021d1a8e9f563eed3c29c4873b6
Mahesh Sabnis <sabnis_m@hotmail.com>
2020-12-22 12:50:46 +0530

    Initial check-in of module rHealApps

---
+++ rHealApps/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rHealApps
+{
+    /// <summary>
+    /// The Application Startup calss
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // the method that provides default
+            // initialization settings for the application
+            // Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(App));
+            // 1. Loads the XAML file in this case App.xaml
+            // 2. Parse the File
+            // 3. Map the XAML tags (?) from file to correcponding .NET Types
+            // 4. Compile them
+            // 5. Execute them
+            InitializeComponent();
+
+            // represent the page to be loaded
+            // when the App s loaded
+            MainPage = new MainPage();
+           
+        }
+
+
+        // Operations to be performed when device boots the appication
+        // Start the app for the first time
+        protected override void OnStart()
+        {
+        }
+
+        // the app is put at lower index in Application Stack
+        // the app enters in sleep mode
+        protected override void OnSleep()
+        {
+        }
+        // When the App resumes from sleep mode
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ rHealApps/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace rHealApps
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> professions;
+        public MainPage()
+        {
+            InitializeComponent();
+            // all initializations in constructor must be done after the the
+            // INitializeComponent() method
+
+            professions = new List<string>();
+            professions.Add("Doctor");
+            professions.Add("Enginerr");
+            professions.Add("Architect");
+            professions.Add("Trainer");
+            professions.Add("Pilot");
+
+            foreach (var item in professions)
+            {
+                lstprofession.Items.Add(item);
+            }
+        }
+
+        void btnClear_Clicked(System.Object sender, System.EventArgs e)
+        {
+            firstName.Text = "";
+            middleName.Text = "";
+            lastName.Text = "";
+        }
+
+        void btnSave_Clicked(System.Object sender, System.EventArgs e)
+        {
+            lblInfo.Text = $"The Data Recived {firstName.Text} {middleName.Text} {lastName.Text} {lstprofession.SelectedItem.ToString()} ";
+        }
+    }
+}
